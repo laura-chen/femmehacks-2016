0bc15cd8c50d3e46f180c10c6818c6a1ef2ccf8e
Laura Chen <lchen5@swarthmore.edu>
2016-02-20 14:54:12 -0500

    Chrome sample now compatible with Firebase sample programs.

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,44 +1,38 @@
 Firebase.enableLogging(true);
+/*var f = new Firebase('https://chrome-sample.firebaseio-demo.com/');
 
-var myFirebaseApp = "chrome-reflect";
-
-// Reference to your entire Firebase database
-var myFirebase = new Firebase("https://" + myFirebaseApp + ".firebaseio.com/");
-
-// Get a reference to the recommendations object of your Firebase.
-// Note: this doesn't exist yet. But when we write to our Firebase using
-// this reference, it will create this object for us!
-var entries = myFirebase.child("entries);
-
-// Push our first recommendation to the end of the list and assign it a
-// unique ID automatically.
-entries.push({
-    "title": "The danger of a single story",
-    "date": "2/6/16",
-    "content": "https://www.ted.com/talks/chimamanda_adichie_the_danger_of_a_single_story"
-});
-
-//var f = new Firebase('https://chrome-sample.firebaseio-demo.com/');
+f.transaction(function(curr) {
+  if (isNaN(parseFloat(curr)))
+    return 1; // initialize to 1.
+  else
+    return curr + 1; // increment.
+}, function() {
+    // Once the transaction has completed, update the UI (and watch for updates).
+    f.on('value', function(s) {
+      document.getElementById('contents').innerHTML = s.val();
+    });
+  });
+*/
 
 // TODO: Replace with your Firebase app
 var myFirebaseApp = "chrome-reflect";
 
-// Reference to the entries object in your Firebase
-var entries = new Firebase("https://" + myFirebaseApp + ".firebaseio.com/entries");
+// Reference to the recommendations object in your Firebase
+var recommendations = new Firebase("https://" + myFirebaseApp + ".firebaseio.com/recommendations");
 
 // Save a new recommendation to the database, using the input in the form
-var submitEntry = function () {
+var submitRecommendation = function () {
 
   // Get input values from each of the form elements
-  var title = $("#entryTitle").val();
-  var date = $("#entryDate").val();
-  var content = $("#entryContent").val();
+  var title = $("#talkTitle").val();
+  var presenter = $("#talkPresenter").val();
+  var link = $("#talkLink").val();
 
   // Push a new recommendation to the database using those values
-  entries.push({
+  recommendations.push({
     "title": title,
-    "date": date,
-    "content": content
+    "presenter": presenter,
+    "link": link
   });
 };
 
@@ -46,18 +40,18 @@ var submitEntry = function () {
 // update the table with its values. This is called every time the child_added
 // event is triggered on the recommendations Firebase reference, which means
 // that this will update EVEN IF you don't refresh the page. Magic.
-entries.limitToLast(1).on('child_added', function(childSnapshot) {
+recommendations.limitToLast(1).on('child_added', function(childSnapshot) {
   // Get the recommendation data from the most recent snapshot of data
   // added to the recommendations list in Firebase
-  entry = childSnapshot.val();
+  recommendation = childSnapshot.val();
 
   // Update the HTML to display the recommendation text
-  $("#title").html(entry.title)
-  $("#date").html(entry.date)
-  $("#content").html(entry.content)
+  $("#title").html(recommendation.title)
+  $("#presenter").html(recommendation.presenter)
+  $("#link").html(recommendation.link)
 
   // Make the link actually work and direct to the URL provided
-  $("#link").attr("href", entry.link)
+  $("#link").attr("href", recommendation.link)
 });
 
 // When the window is fully loaded, call this function.
@@ -69,18 +63,6 @@ $(window).load(function () {
 
   // Find the HTML element with the id recommendationForm, and when the submit
   // event is triggered on that element, call submitRecommendation.
-  $("#entryForm").submit(submitEntry);
+  $("#recommendationForm").submit(submitRecommendation);
 
 });
-
-entries.transaction(function(curr) {
-  if (isNaN(parseFloat(curr)))
-    return 1; // initialize to 1.
-  else
-    return curr + 1; // increment.
-}, function() {
-    // Once the transaction has completed, update the UI (and watch for updates).
-    f.on('value', function(s) {
-      document.getElementById('contents').innerHTML = s.val();
-    });
-  });
